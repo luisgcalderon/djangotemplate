c8514842118c9f1ac55f1c651c53501953b608a5
LuisGCalderon <luis.calderon@anahuac.mx>
2020-10-18 12:07:05 -0500

    Add configuracion settings and admin model form

--- src/django_myproject/myproject/settings/_base.py
+++ src/django_myproject/myproject/settings/_base.py
@@ -62,7 +62,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mproject.apps.magazine'
+    'myproject.apps.magazine'
 ]
 
 MIDDLEWARE = [
