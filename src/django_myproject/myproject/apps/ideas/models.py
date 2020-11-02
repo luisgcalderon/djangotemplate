235216b09e23f3d2d8125fe44c0189f7ddf9ac9a
LuisGCalderon <luis.calderon@anahuac.mx>
2020-11-01 19:06:25 -0600

    Creating model mixin to handle generic relations

--- src/django_myproject/myproject/apps/ideas/models.py
+++ src/django_myproject/myproject/apps/ideas/models.py
@@ -4,7 +4,38 @@ from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 
-from myproject.apps.core.models import UrlBase, CreationModificationDateBase
+from myproject.apps.core.models import (
+    UrlBase, 
+    CreationModificationDateBase,
+    object_relation_base_factory as generic_relation
+)
+
+FavoriteObjectBase = generic_relation(
+    is_required=True
+)
+
+OwnerBase = generic_relation(
+    prefix="owner",
+    prefix_verbose=_("Owner"),
+    is_required=True,
+    add_related_name=True,
+    limit_content_type_choices_to={
+        "model__in": (
+            "user",
+            "group",
+        )
+    }
+)
+
+class Like(FavoriteObjectBase, OwnerBase):
+    class Meta:
+        verbose_name = _("Like")
+        verbose_name_plural = _("Likes")
+    def __str__(self):
+        return _("{owner} likes {object}".format(
+            owner=self.owner_content_object,
+            object=self.content_object
+        ))
 
 class Idea(UrlBase, CreationModificationDateBase):
     title = models.CharField(
